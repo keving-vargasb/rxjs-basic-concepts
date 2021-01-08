97b5b4604945f35a5516d34a02788516dbe4e88a
keving-vargasb <keving.vargasb@gmail.com>
2021-01-07 21:36:27 -0500

    Create custom observable and observer

--- index.ts
+++ index.ts
@@ -1,9 +1,27 @@
-import { of } from 'rxjs'; 
-import { map } from 'rxjs/operators';
+import { Observable, Observer } from "rxjs";
 
+const firstObservable = new Observable(suscriber => {
+  suscriber.next("This is a string");
+  suscriber.next(1);
+  suscriber.next(2);
+  suscriber.error("This is a error");
+});
 
-const source = of('World').pipe(
-  map(x => `Hello ${x}!`)
-);
+const secondObservable = new Observable(suscriber => {
+  suscriber.complete();
+});
 
-source.subscribe(console.log);
+const myObserver: Observer<any> = {
+  next: x => {
+    if (isNaN(x)) {
+      console.warn(`'${x}' is not a number`);
+      return;
+    }
+    console.log(x + 10);
+  },
+  error: err => console.error("There is an error", err),
+  complete: () => console.log("Complete")
+};
+
+firstObservable.subscribe(myObserver);
+secondObservable.subscribe(myObserver);
